799417e5468a9f5f0079b1931ed76b69efa2fd2c
Jake Luecke <jrluecke95@gmail.com>
2021-01-15 12:49:33 -0500

    finihsed phone book app

--- phone_book_app.py
+++ phone_book_app.py
@@ -4,50 +4,42 @@ phonebook = {
     "sara": "333-333-3333"}
 
 
-def look_up(answer):
-    if answer == 1:
-        while True:
-            try:
-                name = input("Who's number do you want to look up? ").lower()
-                print(phonebook[name])
-                break
-            except KeyError:
-                print("sorry, we couldn't find that name, please try again!")
+def look_up():
+    while True:
+        try:
+            name = input("Who's number do you want to look up? ").lower()
+            print(phonebook[name])
+            break
+        except KeyError:
+            print("sorry, we couldn't find that name, please try again!")
         
-def set_entry(answer):
-    if answer == 2:
-        while True:
-            try:
-                name = input("what do you want this persons name to be? ").lower()
-                number = input("what do you want their number to be? - please include dashes ")
-                if (len(name) > 0) and (len(number) == 12):
-                    phonebook[name] = number
-                    break
-                else:
-                    raise Exception("invaild name or number")
-            except:
-                print("Please enter a valid name and number")
-
-def delte_entry(answer):
-    if answer == 3:
-        while True:
-            try:
-                name = input("who's number do you want to delete? ").lower()
-                del phonebook[name]
+def set_entry():
+    while True:
+        try:
+            name = input("what do you want this persons name to be? ").lower()
+            number = input("what do you want their number to be? - please include dashes ")
+            if (len(name) > 0) and (len(number) == 12):
+                phonebook[name] = number
                 break
-            except KeyError:
-                print("sorry, we couldn't find that name")
+            else:
+                raise Exception("invaild name or number")
+        except:
+            print("Please enter a valid name and number")
 
-def list_entries(answer):
-    if answer == 4:
-        for name in phonebook:
-            print(name, phonebook[name])
+def delete_entry():
+    while True:
+        try:
+            name = input("who's number do you want to delete? ").lower()
+            del phonebook[name]
+            break
+        except KeyError:
+            print("sorry, we couldn't find that name")
 
-def quit(answer):
-    if answer == 5:
-        return None
+def list_entries():
+    for name in phonebook:
+        print(name, phonebook[name])
 
-def phonebook_app(answer):
+def phonebook_app():
     print("""
     Electronic Phone Book:
 
@@ -58,3 +50,24 @@ def phonebook_app(answer):
     3. Delete an entry
     4. List all entries
     5. Quit""")
+    while True:
+        try:
+            answer = int(input("which action would you like to peform?"))
+            if answer < 1 or answer > 5:
+                raise Exception
+            else:
+                break
+        except:
+            print("sorry that is not a valid selection, please try again")
+    if answer < 5:
+        if answer == 1:
+            look_up()
+        elif answer == 2:
+            set_entry()
+        elif answer == 3:
+            delete_entry()
+        elif answer == 4:
+            list_entries()
+        phonebook_app()
+
+phonebook_app()
